5eab026c56fd1126a3f0c7f32004ebb8d60b9f06
Birkin James Diana <birkin.diana@gmail.com>
2026-05-15 06:40:34 -0400

    Revert "fix additionalValues to only work if there's a colon separator"
    This reverts commit 2c7e331395bc16d16b512b4a17d3ec4b1e30f910.

--- profileInterpreter.py
+++ profileInterpreter.py
@@ -72,14 +72,13 @@ def getTermsOfAddressPrependAndAppendStripped(name):
     return prependTermOfAddress, appendTermOfAddress
 
 def getAdditionalValues(name):
-    additionalValuesResults = re.findall("\[(.*: .*)\]", name)
+    additionalValuesResults = re.findall("\[(.*)\]", name)
     additionalValues = {}
 
     if len(additionalValuesResults) > 0:
         for result in additionalValuesResults:
             additionalValue = yaml.safe_load(result)
-            if type(additionalValue) is not dict:
-                continue
+
             for key, value in additionalValue.items():
                 additionalValues["entry." + key] = value
 
@@ -161,6 +160,7 @@ def getMetadataFromEntry(entry, method):
     value = normalizeString(entry)
 
     additionalValues, additionalValuesResults = getAdditionalValues(entry)
+    additionalValuesResults = re.findall("(\[.*\])", entry)
     for additionalValuesResult in additionalValuesResults:
         entry = entry.replace(additionalValuesResult, "")
 
